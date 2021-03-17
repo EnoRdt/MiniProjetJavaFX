3b8a2f5064ad93ba1f4bfe94fda068fbb1d4ba90
EnolaRdt <roudautenola@gmail.com>
2021-03-17 15:40:16 +0100

    Creation of different controllers

---
+++ Mini projet/src/sample/AddAndEditStudentController.java
@@ -0,0 +1,10 @@
+package sample;
+
+/**
+ * Class which allows to edit details of a student
+ * @author Florimel Flotté, Salomé Rebours, Enola Roudaut
+ * @version 17/03/2020
+ */
+
+public class AddAndEditStudentController {
+}
---
+++ Mini projet/src/sample/ListStudentController.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Class which allows to display the details of the list of students
+ * @author Florimel Flotté, Salomé Rebours, Enola Roudaut
+ * @version 17/03/2020
+ */
+public class ListStudentController {
+}
