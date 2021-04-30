046999cea244e2692da6fe31f1bf6f4326f824a3
rebours_salome <salomereb@gmail.com>
2021-04-30 12:30:59 +0200

    menu controller

--- Mini projet/src/sample/MenuController.java
+++ Mini projet/src/sample/MenuController.java
@@ -1,9 +1,42 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
 /**
- * Class which allows to manage the menu ????????????????????????????
+ * Class which allows to manage the menu
  * @author Florimel Flotté, Salomé Rebours, Enola Roudaut
  * @version 17/03/2020
  */
 public class MenuController {
+    @FXML
+    private Menu student_list;
+    @FXML
+    private Menu student_add;
+
+    /**
+     * Initialize the controller class.
+     */
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Redirection to the page where a student can be added
+     */
+    @FXML
+    private void redirectStudentAdd() {
+
+    }
+
+    /**
+     * Redirection to the page where there is the student list
+     */
+    @FXML
+    private void redirectStudentList() {
+
+    }
 }
